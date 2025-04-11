ce68839f4297617ac16bd34ac678eaa6a9b4c7c3
Raphael Rodrigues <raphael_rodrigues2012@hotmail.com>
2025-04-11 13:57:17 -0300

    Update curve_generator.py
    3D Plotting added
    2D Plotting refined

--- src/curve_generator.py
+++ src/curve_generator.py
@@ -10,6 +10,7 @@ from multiprocessing import Pool
 from gzip import open as gsopen
 from os import makedirs
 from collections import defaultdict
+from mpl_toolkits.mplot3d import Axes3D #type: ignore
 
 states:dict[str, str] = {
     "12": "AC", "27": "AL", "13": "AM", "16": "AP", "29": "BA", "23": "CE", "53": "DF",
@@ -18,70 +19,24 @@ states:dict[str, str] = {
     "11": "RO", "14": "RR", "42": "SC", "35": "SP", "28": "SE", "17": "TO"
 }
 
-def average_year_radiation(timeseries:list[str]):
-    #year:np.ndarray = np.asarray([(int(line.split(',')[0][:8]), sum([float(j) for j in line.split(',')[1:4]])) for line in timeseries])
-    
-    year:defaultdict[str, list[float]] = defaultdict(list[float])
-    for line in timeseries:
-        year[line.split(',')[0][4:8]].append(sum([float(j) for j in line.split(',')[1:4]]))
-
-    x:np.ndarray = np.array([int(k) for k in sorted(year.keys())]).T
-    y:np.ndarray = np.array(list(range(24)))
-    #z:np.ndarray = np.array()
-    z = np.asarray([[sum([year[day][j] for j in range(i, len(year[day]), 24)])/(len(year[day])/24) for i in range(24)] for day in sorted(year.keys())])
-    z = np.concatenate([z[:, 3:], z[:, :3]], axis=1)
-    #print(year['0101'])
-    print(x.shape, y.shape, z.shape)
-    import matplotlib.pyplot as plt
-    #from matplotlib import use
-    #use("Agg")
-
-    ax = plt.axes(projection='3d')
-    X, Y = np.meshgrid(x,y)
-    #ax.plot_surface(z)
-    ax.plot_surface(X.T, Y.T, z, cmap='viridis')
-    plt.show()
-
-def curve_gen(data:list[str], orig_coord:list[float], coord:np.ndarray, loss:float) -> None:
-
-    timeseries_coords_folder:Path = Path('%s\\data\\timeseries'%(Path(dirname(abspath(__file__))).parent))
-
-    geocode:str = data[1]
-    state:str = states[geocode[:2]]
-    ceg:str = data[0]
-    #analisar o que deve ser consiferado como power e determinar loss
+def avarage_month_radiation_generation(timeseries:list[str], state:str, geocode:str, ceg:str, orig_coord:list[float], data:list[str], time_correction:int, city_plot_folder:Path):
+    loss:float = 0.14
     power:float = float(float('.'.join(data[4].split(','))))*1000*(1-loss)
-    area:float = float('.'.join(data[6].split(',')))*1.65
-
-    coords_path:Path = Path("%s\\%s\\[%s]\\%s"%(timeseries_coords_folder, state, geocode,
-                                                                next(f for f in listdir("%s\\%s\\[%s]"%(timeseries_coords_folder, state, geocode)) if f[19:].startswith("(%.6f,%.6f)"%(coord[1], coord[0])))))
+    panels:int = int(data[6])
+    area:float = float('.'.join(data[5].split(',')))
     
-    with gsopen(coords_path, 'rt') as f:
-        lines:list[str] = f.readlines()[9:-12]
-
-    average_year_radiation(lines)
-    """ #AAAA-MM-DD" -> AAAAMMDD
+    #AAAA-MM-DD" -> AAAAMMDD
     month:str = ''.join(data[27].split('-')[:-1])
+    if (not(int(timeseries[0][:4])<=int(month[:4])<=int(timeseries[-1][:4]))):
+        month = timeseries[-1][:4]+month[4:]
 
-    if (not(int(lines[0][:4])<=int(month[:4])<=int(lines[-1][:4]))):
-        month = lines[-1][:4]+month[4:]
+    timeseries = [line for line in timeseries if line.startswith(month[:6])]
+    timeseries = timeseries[time_correction:]+timeseries[:time_correction]
 
-    if (state == "AC"):
-        time_correction:int = 5
-    elif (state == "AM"):
-        time_correction = 4
-    elif (geocode == "2605459"):
-        time_correction = 2
-    else:
-        time_correction = 3
-
-    lines = [line for line in lines if line.startswith(month[:6])]
-    lines = lines[time_correction:]+lines[:time_correction]
-
-    month_radiation:list[list[float]] = [[0,0] for _ in lines]
-    for i in range(len(lines)):
+    month_radiation:list[list[float]] = [[0,0] for _ in timeseries]
+    for i in range(len(timeseries)):
         #time,Gb(i),Gd(i),Gr(i),H_sun,T2m,WS10m,Int
-        elements:list[float] = [float(e) for e in lines[i].split(',')[1:]]
+        elements:list[float] = [float(e) for e in timeseries[i].split(',')[1:]]
         month_radiation[i][0] = sum(elements[:3])
         tc:float = (elements[4]+(sum(elements[:3])*25/800)-25)
         tc = tc if tc>0 else 0
@@ -99,31 +54,96 @@ def curve_gen(data:list[str], orig_coord:list[float], coord:np.ndarray, loss:flo
     hourly_generation:list[float] = [sum([month_generation[k][0] for k in range(i, len(month_generation), 24)])/(len(month_generation)//24) for i in range(24)]
 
     # v2:
-    radiation_energy:float = sum(hourly_radiation)/1000 # [kWh/m²]
-    generated_energy:float = sum(hourly_generation)/1000 # [kWh]
-    loss = sum([e[1] for e in month_generation])/len(month_generation)
-    correction_factor1:float = generated_energy/radiation_energy
-    correction_factor2:float = power*(1-loss)/(radiation_energy*1000)
+    radiation_energy:float = sum(hourly_radiation)/1000*area # [kWh/m²]
+    generated_energy1:float = sum(hourly_generation)/1000 # [kWh]
+    loss1:float = sum([e[1] for e in month_generation])/len(month_generation)
+    correction_factor1:float = power*loss1/1000 #generated_energy1/radiation_energy
+    loss2:float = 0.14
+    correction_factor2:float = power*(1-loss2)/(radiation_energy*1000)*area
+    generated_energy2:float = sum(hourly_radiation)*correction_factor2/1000
 
 
     import matplotlib.pyplot as plt
     from matplotlib import use
     use("Agg")
 
-    makedirs("%s\\curves\\%s\\[%s]"%(ventures_folder, state, geocode), exist_ok=True)
+    fig, ax1 = plt.subplots()
 
-    plt.plot(range(24), [hourly_radiation[i]/1000 for i in range(24)], label="Radiation Energy Curve", color="black")
-    #plt.bar(range(24), [hourly_generation[i]/1000 for i in range(24)], label="Produced Energy 1", color="#DB5C1F")
-    #plt.bar(range(24), [hourly_radiation[i]*correction_factor2/1000 for i in range(24)], label="Produced Energy 2", color="blue")
+    ax2 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
+    
+    ax2.plot(range(24), [hourly_radiation[i]/1000 for i in range(24)], label="Irradiance Curve", color="black") #radiation
+    ax1.bar(range(24), [hourly_generation[i]/1000 for i in range(24)], label="Method 1", color="#DB5C1F") # 1sr formula
+    ax1.bar(range(24), [hourly_radiation[i]*correction_factor2/1000 for i in range(24)], label="Method 2", color="#1F2792", alpha=0.70) #2nd formula
     plt.title(ceg)
-    plt.suptitle("Power curve at (%f,%f), [%s]\n\nArea: %.0f m²    Power: %.0f kW    Loss: %.2f %%"%(orig_coord[1], orig_coord[0], geocode, area, power/1000, loss*100))
-    plt.xlabel("Time [Hour]\n\nFactors: (%.3f, %.3f)    Radiation Energy: %.2f kWh/m²    Produced Energy: %.2f kWh"%(correction_factor1, correction_factor2, radiation_energy, generated_energy))
-    plt.ylabel("Power [kW]")
-    plt.legend()
-    plt.tight_layout()
-    plt.savefig("%s\\curves\\%s\\[%s]\\%s.png"%(ventures_folder, state, geocode, ceg), backend='Agg', dpi=200)
+    plt.suptitle("PV Yield at (%f,%f), [%s]\n\nArea: %.0f m²    Panels: %i    Power: %.0f kW"%(orig_coord[1], orig_coord[0], geocode, area, panels, power/1000))
+    ax1.set_xlabel("Time [Hour]\n\nRadiation Energy: %.2f kWh\n\nMethod 1 - Factor: %.3f    Produced Energy: %.2f kWh    Loss: %.2f %%\n\nMethod 2 - Factor: %.3f    Produced Energy: %.2f kWh    Loss: %.2f %%"%(radiation_energy, correction_factor1, generated_energy1, loss1*100, correction_factor2, generated_energy2, loss2*100))
+    ax1.set_ylabel("Energy [kWh]", color='#DB5C1F')
+    ax2.set_ylabel("Irradiance [kWh/m²]", color='black')
+    ax1.set_ylim(ax2.set_ylim())
+    ax1.legend(loc=2)
+    ax2.legend(loc=1)
 
-    plt.close() """
+    fig.tight_layout()
+    plt.savefig("%s\\%s.png"%(city_plot_folder, ceg), backend='Agg', dpi=200)
+
+    plt.close()
+
+
+def average_year_radiation(timeseries:list[str], ceg:str, time_correction:int, city_plot_folder:Path):
+    year:defaultdict[str, list[float]] = defaultdict(list[float])
+    for line in timeseries:
+        year[line.split(',')[0][4:8]].append(sum([float(j) for j in line.split(',')[1:4]]))
+
+    x:np.ndarray = np.array(list(range(1, 367))).T
+    y:np.ndarray = np.array(list(range(24)))
+    #z:np.ndarray = np.array()
+    z = np.asarray([[sum([year[day][j] for j in range(i, len(year[day]), 24)])/(len(year[day])/24) for i in range(24)] for day in sorted(year.keys())])
+    z = np.concatenate([z[:, time_correction:], z[:, :time_correction]], axis=1)
+    #print(year['0101'])
+    #print(x.shape, y.shape, z.shape)
+    
+    import matplotlib.pyplot as plt
+    from matplotlib import use
+    use("Agg")
+
+    ax:Axes3D = plt.axes(projection='3d')
+    X, Y = np.meshgrid(x,y)
+    #ax.plot_surface(z)
+    ax.plot_surface(X.T, Y.T, z, cmap='viridis')
+    ax.view_init(20, -50, 0)
+    #plt.show()
+    plt.savefig("%s\\%s-3D-year-radiation.png"%(city_plot_folder, ceg), backend='Agg', dpi=200)
+    plt.close()
+    #22, -33, 0
+
+def curve_gen(data:list[str], orig_coord:list[float], coord:np.ndarray, loss:float) -> None:
+
+    timeseries_coords_folder:Path = Path('%s\\data\\timeseries'%(Path(dirname(abspath(__file__))).parent))
+
+    geocode:str = data[1]
+    state:str = states[geocode[:2]]
+    ceg:str = data[0]
+
+    if (state == "AC"):
+        time_correction:int = 5
+    elif (state == "AM"):
+        time_correction = 4
+    elif (geocode == "2605459"):
+        time_correction = 2
+    else:
+        time_correction = 3
+
+    coords_path:Path = Path("%s\\%s\\[%s]\\%s"%(timeseries_coords_folder, state, geocode,
+                                                                next(f for f in listdir("%s\\%s\\[%s]"%(timeseries_coords_folder, state, geocode)) if f[19:].startswith("(%.6f,%.6f)"%(coord[1], coord[0])))))
+    
+    with gsopen(coords_path, 'rt') as f:
+        lines:list[str] = f.readlines()[9:-12]
+    
+    city_plot_folder:Path = Path("%s\\outputs\\plot\\%s\\[%s]"%(Path(dirname(abspath(__file__))).parent, state, geocode))
+    makedirs("%s"%(city_plot_folder), exist_ok=True)
+    
+    average_year_radiation(lines, ceg, time_correction, city_plot_folder)
+    avarage_month_radiation_generation(lines, state, geocode, ceg, orig_coord, data, time_correction, city_plot_folder)
 
 def gds_generation_curve(sts:list[str] = [], geocodes:list[str] = [], loss:float=0.0) -> None:
     t0:float = perf_counter()
@@ -183,7 +203,7 @@ def gds_generation_curve(sts:list[str] = [], geocodes:list[str] = [], loss:float
                 """ cts = np.concatenate((city_ventures_coords, city_coords[idxs], np.reshape(distances, [len(distances),1])), 1)
                 print(cts, sep="\n") """
                 
-                p.starmap(curve_gen, [[line[1:-2].split('";"'), orig_coord, closest_timeseries, loss] for (line, orig_coord, closest_timeseries) in zip(lines[:1], city_ventures_coords, city_coords[idxs])])
+                p.starmap(curve_gen, [[line[1:-2].split('";"'), orig_coord, closest_timeseries, loss] for (line, orig_coord, closest_timeseries) in zip(lines, city_ventures_coords, city_coords[idxs])])
 
     print(perf_counter()-t0)
 
